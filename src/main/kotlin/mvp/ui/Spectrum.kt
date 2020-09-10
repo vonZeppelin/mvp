5f09404f4528f1086d91ecf0c557a0fcd51865c0
Leonid Bogdanov <leonid_bogdanov@mail.ru>
2020-09-10 17:54:42 +1000

    Dummy Spectrum implementation

--- src/main/kotlin/mvp/ui/Spectrum.kt
+++ src/main/kotlin/mvp/ui/Spectrum.kt
@@ -1,25 +1,56 @@
 package mvp.ui
 
+import javafx.animation.KeyFrame
+import javafx.animation.Timeline
+import javafx.event.ActionEvent
+import javafx.event.EventHandler
 import javafx.scene.canvas.Canvas
-import javafx.scene.paint.Color
+import javafx.scene.paint.Color.BLACK
+import javafx.util.Duration
+import kotlin.random.Random
+import mvp.audio.Player
 
-class Spectrum : Canvas() {
+private const val BARS_COUNT = 25
+private const val BARS_GAP = 2
 
-    override fun isResizable(): Boolean = true
+class Spectrum : Canvas(), EventHandler<ActionEvent> {
+    private val timeline = Timeline(KeyFrame(Duration.millis(45.0), this)).apply {
+        cycleCount = Timeline.INDEFINITE
+        isAutoReverse = true
+    }
 
-    override fun maxHeight(width: Double): Double = Double.POSITIVE_INFINITY
-    override fun minHeight(width: Double): Double = 1.0
-    override fun maxWidth(width: Double): Double = Double.POSITIVE_INFINITY
-    override fun minWidth(width: Double): Double = 1.0
+    init {
+        graphicsContext2D.fill = BLACK
 
-    override fun resize(width: Double, height: Double) {
-        this.width = width
-        this.height = height
-
-        with(graphicsContext2D) {
-            stroke = Color.AQUAMARINE
-            strokeRoundRect(1.0, 1.0, width - 1, height - 1, 1.0, 1.0)
+        Player.statusProperty.addListener { _, _, newValue ->
+            if (newValue == Player.Status.PLAYING) timeline.play() else timeline.stop()
         }
     }
 
+    override fun maxHeight(width: Double): Double = Double.POSITIVE_INFINITY
+    override fun minHeight(width: Double): Double = 1.0
+
+    override fun maxWidth(width: Double): Double = Double.POSITIVE_INFINITY
+    override fun minWidth(width: Double): Double = 1.0
+
+    override fun isResizable(): Boolean = true
+    override fun resize(width: Double, height: Double) {
+        this.width = width
+        this.height = height
+    }
+
+    override fun handle(event: ActionEvent) {
+        if (width == 0.0 || height == 0.0) return
+
+        val fft = List(BARS_COUNT) { Random.nextDouble(0.0, height) }
+        val barWidth = (width - BARS_GAP * (BARS_COUNT - 1)) / BARS_COUNT
+
+        with(graphicsContext2D) {
+            clearRect(0.0, 0.0, width, height)
+
+            fft.forEachIndexed { index, value ->
+                fillRect((barWidth + BARS_GAP) * index, value, barWidth, height)
+            }
+        }
+    }
 }
